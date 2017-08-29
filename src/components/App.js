bcb6fb2e6ce0b67268aa90b08199c148978f6957
Ryan Thames <ryanthames@gmail.com>
2017-08-28 20:21:31 -0500

    Wow, state in react is confusing

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes }); // identical to this.setState({ fishes: fishes})
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +34,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
