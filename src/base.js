02564e1242403d76614e508952680d6a3164bcdd
Ryan Thames <ryanthames@gmail.com>
2017-09-15 20:42:50 -0500

    Add firebase

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyAPDeyO3f27XUUfPO18QS2B9tYL_NvFwb8",
+    authDomain: "catch-of-the-day-ryan-thames.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-ryan-thames.firebaseio.com"
+});
+
+export default base;
--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ import Fish from './Fish';
 
 import sampleFishes from '../sample-fishes';
 
+import base from '../base';
+
 class App extends React.Component {
   constructor() {
     super();
@@ -20,6 +22,17 @@ class App extends React.Component {
       fishes: {},
       order: {}
     };
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`, {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
 
   addFish(fish) {
